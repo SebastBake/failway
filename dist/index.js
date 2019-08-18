6c9f6658c9ff5597fe973af92d24475b8a2f9f53
Sebatian Baker <sebast.bake@gmail.com>
2019-08-18 18:19:44 +1000

    Remove dist

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./maybe"));
-var rails_1 = require("./rails");
-exports.Rail = rails_1.Rail;
